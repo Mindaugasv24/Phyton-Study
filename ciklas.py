19ff1351509ccf9358cde44d3961d5c19de6c1ab
Mindaugasv24 <165074498+Mindaugasv24@users.noreply.github.com>
2024-03-27 18:30:38 +0200

    ciklas.py

---
+++ ciklas.py
@@ -0,0 +1,24 @@
+# ------------------------------- Ciklas While and for ----------------------------------
+# import time
+# i = 10
+
+# while i < 15:
+    # print(i)
+    # i += 1
+    # time.sleep(1)
+    # if i == 15:
+    #     break
+# else:
+#     print('spausdinti else')
+
+
+# for i in range(10):
+    # print(i)
+    # if i == 8:
+    #     break
+    # if i == 5:
+    #     continue
+    # print(i)
+
+# -------------------------------------  Uzdaviniai: 24/03/27
+
