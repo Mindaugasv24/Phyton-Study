99e56e50c621d35c1e8500316a133306ea7d1147
Mindaugasv24 <165074498+Mindaugasv24@users.noreply.github.com>
2024-04-02 18:04:47 +0300

    list_comprehension_and_functions.py

--- list_comprehension and functions.py
+++ list_comprehension and functions.py
@@ -107,12 +107,12 @@ def is_perfect_square(number):
 # print(is_perfect_square(15))
 # print(is_perfect_square(81))
 
-
 # -----------------------------------------Destytojo uzdavinys:------------------------
 # Lentynoje sudeti batai:
 # [8.90, 4,90, 13,20, 8,80, 14,00, 12,00]
 # a) Paskaičiuokite kiek eurų liks žmogui, jei jis šiuo metu pirks dvejus pigiausius batus;
 # b) kokius dvejus batus jam nupirkti, jei jis turi 20 eurų ir nori, kad jam liktų kuo mažiau eurų
+
 
 
 
