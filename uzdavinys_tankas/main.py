d6788e4328d2cd97c685303108ee11114d567c7f
Mindaugasv24 <165074498+Mindaugasv24@users.noreply.github.com>
2024-04-25 21:03:56 +0300

    changes

--- uzdavinys_tankas/main.py
+++ uzdavinys_tankas/main.py
@@ -1,16 +1,34 @@
 class Tank:
-    def __init__(self, x=int, y=int, cordinates=int):
-        self.x = x
-        self.y = y
-        self.cordinates = cordinates
+    """representing game program name"""
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.direction = 'N'
+        self.shots_fired = {'N': 0, 'E': 0, 'S': 0, 'W': 0}
 
     def check_cordinates(self):
+        """representing board of cordinates"""
         for x in range(10):
             for y in range(10):
-                if x == self.cordinates_x and y == self.cordinates_y:
-                    print(">", end="")
-                elif x == self.cordinates_x and y == self.cordinates_y:
-                    print("0", end="")
+                if x == self.x and y == self.y:
+                    print("^", end="")
+                # elif x == self.x and y == self.y:
+                #     print("0", end="")
                 else:
                     print("_", end="")
             print()
+
+    def forward(self):
+        """representing tank move"""
+        if self.direction == 'N':
+            self.y += 1
+        elif self.direction == 'E':
+            self.x += 1
+        elif self.direction == 'S':
+            self.y -= 1
+        elif self.direction == 'W':
+            self.x -= 1
+
+
+tank = Tank()
+tank.check_cordinates()
