1772981f7dba79ee64a004730b77bd3413a504cc
Mindaugasv24 <165074498+Mindaugasv24@users.noreply.github.com>
2024-04-29 18:46:27 +0300

    changes

--- modules.py
+++ modules.py
@@ -1,5 +1,11 @@
-listas = [4, 5, 6]
-listas2 = ['dakjf']
-listas.append(listas2)
-
-print(listas[3][0][1::2])
+# listas = [4, 5, 6]
+# listas2 = ['dakjf']
+# listas.append(listas2)
+# print(listas[3][0][1::2])
+"""
+Kauliukų žaidimas:
+Kauliukų gali būti nuo 1 iki 5 – vartotojas pasako;
+Kiekvienas kauliukas gali turėti puselių nuo 1-100 – vartotojas pasako;
+Žaidimas – ridenu kauliukus;
+Įstorija kaupiama 10 kartų.
+"""
