ef7fa2d3c31b2e0b987e9325ecdd2d19b880e7d0
Mindaugasv24 <165074498+Mindaugasv24@users.noreply.github.com>
2024-04-29 20:32:00 +0300

    changes

--- modules.py
+++ modules.py
@@ -1,4 +1,4 @@
-# listas = [4, 5, 6]
-# listas2 = ['dakjf']
-# listas.append(listas2)
-# print(listas[3][0][1::2])
+listas = [4, 5, 6]
+listas2 = ['adidfy']
+listas.append(listas2)
+print(listas[3][0][1::2])
