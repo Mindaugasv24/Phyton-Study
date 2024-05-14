6aff4e9a73dd9595e2d5d44ad5225442f14b6fe8
Mindaugasv24 <165074498+Mindaugasv24@users.noreply.github.com>
2024-05-14 21:35:06 +0300

    changes

--- db/db_lesson.py
+++ db/db_lesson.py
@@ -25,10 +25,64 @@ class DB:
 
     def add_data_transmision(self, **kwargs):
         """representing data of transmision"""
-        sql_query = (
-            f"Insert into (transmision, transmision_type) "
-            f'values ("{kwargs["transmision"]}", "{kwargs["transmision_type"]}")'
-        )
+        sql_query = f"""
+            Insert into transmision (transmision, transmision_type) values
+            ("{kwargs["transmision"]}", "{kwargs["transmision_type"]}");
+        """
+        print(sql_query)
+        self.execute_sql_query(sql_query)
+
+    def add_data_market(self, **kwargs):
+        """representing data of market"""
+        # price = kwargs.get("price", 40000)
+        # make_year = kwargs.get("make_year", 1964)
+        # color = kwargs.get("color", None)
+        # Milieage_run = kwargs.get("Milieage_run", 25000)
+        sql_query = f"""
+            Insert into market (Price, make_year, color, Milieage_run) values
+            ("{kwargs["price"]}", "{kwargs["make_year"]}", "{kwargs["color"]}", "{kwargs["Milieage_run"]}");
+        """
+        print(sql_query)
+        self.execute_sql_query(sql_query)
+
+    def add_data_engine(self, **kwargs):
+        """representing data of engine"""
+        # engine_type = kwargs.get("engine_type", "petrol"),
+        # cc_displacement = kwargs.get("cc_displacement", "petrol"),
+        # Power_BHP = kwargs.get("Power_BHP", "240"),
+        # Torque_Nm = kwargs.get("Torque_Nm", "240"),
+        # Fuel_type = kwargs.get("Fuel_type", "petrol"),
+        sql_query = f"""
+            Insert into engine (engine_type, cc_displacement, Power_BHP, Torque_Nm, Fuel_type) values
+            ("{kwargs["engine_type"]}", "{kwargs["cc_displacement"]}", "{kwargs["Power_BHP"]}", "{kwargs["Torque_Nm"]}",
+            "{kwargs["Fuel_type"]}");
+        """
+        print(sql_query)
+        self.execute_sql_query(sql_query)
+
+    def add_data_model_type(self, **kwargs):
+        """representing data of model_type"""
+        # make = (kwargs.get("make", 1945),)
+        # body_type = (kwargs.get("body_type", "sedan"),)
+        # seating_capacity = (kwargs.get("seating_capacity", 4),)
+        # fuel_tank_capacity_L = kwargs.get("fuel_tank_capacity_L", 70)
+        sql_query = f"""
+            Insert into model_type (make, body_type, seating_capacity, fuel_tank_capacity_L) values
+            ("{kwargs["make"]}", "{kwargs["body_type"]}", "{kwargs["seating_capacity"]}",
+            "{kwargs["fuel_tank_capacity_L"]}");
+        """
+        print(sql_query)
+        self.execute_sql_query(sql_query)
+    def add_data_model(self, **kwargs):
+        """representing data of model"""
+        # Car_name = kwargs.get("Car_name", "mustang")
+        # Mileage_kmpl = kwargs.get("Mileage_kmpl", 180)
+        # model = kwargs.get("model", "shelby GT 500")
+        sql_query = f"""
+                    Insert into model (Car_name, Mileage_kmpl, model) values
+                    ("{kwargs["Car_name"]}", "{kwargs["Mileage_kmpl"]}", "{kwargs["model"]}");
+        """
+        print(sql_query)
         self.execute_sql_query(sql_query)
 
     def drop_tables_if_exits(self, table):
--- main.py
+++ main.py
@@ -6,24 +6,99 @@ db.create_connection_if_not_exist()
 tables = [
     "CREATE TABLE IF NOT EXISTS transmision (transmision_id INTEGER PRIMARY KEY AUTOINCREMENT, transmision char, transmision_type char);",
     "CREATE TABLE IF NOT EXISTS engine(engine_id INTEGER PRIMARY KEY AUTOINCREMENT, engine_type char, cc_displacement char, Power_BHP integer, Torque_Nm integer, Fuel_type char);",
-    "CREATE TABLE IF NOT EXISTS model_type(model_type_id INTEGER PRIMARY KEY AUTOINCREMENT, Make char, Body_type char, Seating_capacity integer, Fuel_tank_capacity_L integer);",
+    "CREATE TABLE IF NOT EXISTS model_type(model_type_id INTEGER PRIMARY KEY AUTOINCREMENT, make char, body_type char, seating_capacity integer, fuel_tank_capacity_L integer);",
     "CREATE TABLE IF NOT EXISTS model(model_id INTEGER PRIMARY KEY AUTOINCREMENT, Car_name char, Mileage_kmpl integer, model char);",
-    "CREATE TABLE IF NOT EXISTS market(market_id INTEGER PRIMARY KEY AUTOINCREMENT, Price integer, Make_year integer, color char,"
-    "Milieage_run integer, No_of_owners char);",
+    "CREATE TABLE IF NOT EXISTS market(market_id INTEGER PRIMARY KEY AUTOINCREMENT, price integer, make_year integer, color char, Milieage_run integer, no_of_owners char);",
 ]
-for table in tables:
-    db.execute_sql_query(table)
+# for table in tables:
+#     db.execute_sql_query(table)
 
 # tables2 = ["transmision", "engine", "model_type", "model", "market"]
 # for table in tables2:
 #     db.drop_tables_if_exits(table)
 
-db.close_connection()
+# db.close_connection()
 
-"""
-INSERT INTO transmision (transmision, transmision_type) VALUES ("7 belekas", "automatic");
-INSERT INTO engine (engine_type, cc_displacement, Power_BHP, Torque_Nm, Fuel_type) VALUES ("v8", "sda", "245", "fa", "petrol");
-INSERT INTO model_type (Make, Body_type, Seating_capacity, Fuel_tank_capacity_L) VALUES ("1964", "sedan", "2" "60");
-INSERT INTO model (Car_name, Mileage_kmpl, model) VALUES ("mustang", "180", "shelby GT 500");
-INSERT INTO market (Price, Make_year, color, Milieage_run, No_of_owners) VALUES ("40000", "1964", "red", "25000", "1");
-"""
+transmisions = [
+    {"transmision": "six gear", "transmision_type": "aumatik"},
+    # {"transmision": "four gear", "transmision_type": "manual"},
+    # {"transmision": "five gear", "transmision_type": "aumatik"},
+    # {"transmision": "two gear", "transmision_type": "aumatik"},
+]
+for i in transmisions:
+    db.add_data_transmision(**i)
+
+markets = [
+    {
+        "price": 40000,
+        "make_year": 1964,
+        "color": "red",
+        "Milieage_run": 25000,
+    },
+    {
+        "price": 45000,
+        "make_year": 1955,
+        "color": "blue",
+        "Milieage_run": 22000,
+    },
+    {
+        "price": 48000,
+        "make_year": 1974,
+        "color": "yellow",
+        "Milieage_run": 2000,
+    },
+    {
+        "price": 35000,
+        "make_year": 1994,
+        "color": "black",
+        "Milieage_run": 21000,
+    },
+]
+for i in markets:
+    db.add_data_market(**i)
+
+engines = [
+    {
+        "engine_type": "petrol",
+        "cc_displacement": "petrol",
+        "Power_BHP": 240,
+        "Torque_Nm": 240,
+        "Fuel_type": "petrol",
+    },
+    {
+        "engine_type": "petrol",
+        "cc_displacement": "petrol",
+        "Power_BHP": 240,
+        "Torque_Nm": 240,
+        "Fuel_type": "petrol",
+    },
+    {
+        "engine_type": "petrol",
+        "cc_displacement": "petrol",
+        "Power_BHP": 240,
+        "Torque_Nm": 240,
+        "Fuel_type": "petrol",
+    },
+]
+
+for i in engines:
+    db.add_data_engine(**i)
+
+model_types = [
+    { "make": 1945, "body_type": "sedan", "seating_capacity": 4, "fuel_tank_capacity_L": 70},
+    { "make": 1945, "body_type": "sedan", "seating_capacity": 4, "fuel_tank_capacity_L": 70},
+    { "make": 1945, "body_type": "sedan", "seating_capacity": 4, "fuel_tank_capacity_L": 70},
+    { "make": 1945, "body_type": "sedan", "seating_capacity": 4, "fuel_tank_capacity_L": 70},
+]
+
+for i in model_types:
+    db.add_data_model_type(**i)
+
+models = [
+    {"Car_name": "mustang", "Mileage_kmpl": 190, "model": "shelby GT 500"},
+    {"Car_name": "dodge", "Mileage_kmpl": 200, "model": "chalenger"},
+    {"Car_name": "mazda", "Mileage_kmpl": 260, "model": 3},
+]
+
+for i in models:
+    db.add_data_model(**i)
