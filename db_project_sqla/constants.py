890473f20401f9b3987ffbe45c583bc40db3a650
Mindaugasv24 <165074498+Mindaugasv24@users.noreply.github.com>
2024-05-16 17:50:47 +0300

    changes

--- db_project_sqla/constants.py
+++ db_project_sqla/constants.py
@@ -1,1 +1,12 @@
-URL = 'sqlite:///project.db'
+URL = 'database/project.db'
+
+transmision_table = """
+CREATE  TABLE IF NOT EXISTS transmision (
+transmision_id INTEGER PRIMARY KEY,
+transision char,
+transision_type char);
+"""
+
+TABLES = [transmision_table]
+
+DROP_TABLES = ['transision', 'engine', 'model_type', 'model', 'market']
